1c8b4eef7b5fbf3207822e692d411d2cfa8d1725
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 10:37:23 +0500

    code added

---
+++ question_01.js
@@ -0,0 +1,7 @@
+// Write a function that prints the value of a variable declared with var after the function
+// declaration. What will be the output?
+function check(){
+    console.log(a);
+    var a = 5 ;
+}
+check()
