7e19af6baee87ff45a9f559e884497b0fbb8e9a5
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 10:47:52 +0500

    code of question 4 added

---
+++ question_04.js
@@ -0,0 +1,8 @@
+// Write a function that prints the value of a variable assigned to an undeclared variable
+// after the function declaration. What will be the output?
+function check(){
+    const c  = 5 ;
+    console.log(cc);
+    
+}
+check()
