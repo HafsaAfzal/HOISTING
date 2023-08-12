8ad38e5d365e664db04396547eb10bb612eae6af
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 10:41:39 +0500

    code of question 3 added

---
+++ question_03.js
@@ -0,0 +1,7 @@
+// Write a function that prints the value of a variable declared with const after the function
+// declaration. What will be the output?
+function check(){
+    console.log(c);
+    const c  = 5 ;
+}
+check()
