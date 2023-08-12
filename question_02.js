dd615c3a391ddd072abd799ccad427471cf15cc3
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 10:40:27 +0500

    code of question 2 added

---
+++ question_02.js
@@ -0,0 +1,7 @@
+// Write a function that prints the value of a variable declared with let after the function
+// declaration. What will be the output?
+function check(){
+    console.log(b);
+    let b = 5 ;
+}
+check()
